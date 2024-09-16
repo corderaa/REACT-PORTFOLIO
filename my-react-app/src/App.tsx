e72c976814e06b40f374d712436a8b221a1ebc5e
corderaa <ugaitzcordero1@gmail.com>
2024-09-16 16:03:21 +0200

    [COMMIT] Added new component: AboutMe

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,4 +1,5 @@
 import Nav from "./components/myNavbar"
+import AboutMe from "./components/aboutMe"
 import './App.css'
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Nav/>
+      <AboutMe/>
     </>
   )
 }
--- my-react-app/src/components/myNavbar.tsx
+++ my-react-app/src/components/myNavbar.tsx
@@ -3,11 +3,11 @@ import "./../index.css";
 
 function myNavbar() {
   return (
-    <div className="flex flex-wrap items-center justify-between">
-      <h1>Ugaitz Cordero</h1>
+    <div className="flex flex-wrap items-center justify-between font-light p-8 bg-neutral-900/[0.5] rounded-md">
+      <h1 className=" w-60 text-xl">Corderaa</h1>
       <nav className="flex space-x-12">
           <li>
-            <a href="#">Who Am I</a>
+            <a href="#">About Me</a>
           </li>
           <li>
             <a href="#">Learning</a>
