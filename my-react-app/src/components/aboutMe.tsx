107d8536b5cb92225b263f6edc72b04b0ea8a301
corderaa <ugaitzcordero1@gmail.com>
2024-09-16 17:29:48 +0200

    [COMMIT] Added shadow box to text

--- my-react-app/src/components/aboutMe.tsx
+++ my-react-app/src/components/aboutMe.tsx
@@ -8,7 +8,7 @@ function aboutMe(){
                 <p className="font-semibold text-3xl">Ugaitz Cordero Sanchez</p>
             </header>
             <div className="mb-72">
-                <p>I'm a DAM / Multiplataform Aplication Development student from Bilbao, Spain 🌍</p>
+                <p>I'm a DAM / Multiplataform Aplication Development student from Bilbao, Spain</p>
                 <p>Currently chasing my studies to be a great developer</p>
             </div>
         <a href="#"><svg xmlns="http://www.w3.org/2000/svg" id="arrow-circle-down" viewBox="0 0 24 24" width="40" height="40" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs"><g transform="matrix(1,0,0,1,0,0)"><path d="M12,23c-.4,0-.777-.156-1.061-.439L.112,11.733l.707-.707,10.827,10.827c.189,.189,.518,.189,.707,0l10.827-10.827,.707,.707-10.827,10.827c-.283,.283-.66,.439-1.061,.439Zm.471-9.229L23.888,2.354l-.707-.707L12,12.827,.819,1.646,.112,2.354,11.53,13.771c.129,.129,.299,.193,.469,.193s.341-.064,.472-.194Z" fill="#ffffff" fill-opacity="1" data-original-color="#000000ff" stroke="none" stroke-opacity="1"/></g></svg></a>
--- my-react-app/src/components/myNavbar.tsx
+++ my-react-app/src/components/myNavbar.tsx
@@ -6,17 +6,17 @@ function myNavbar() {
     <div className="flex flex-wrap items-center justify-between font-light p-8 bg-neutral-900/[0.5] rounded-md">
       <h1 className=" w-60 text-xl">Corderaa</h1>
       <nav className="flex space-x-12">
-          <li>
-            <a href="#">About Me</a>
+          <li className="">
+            <a className="nav-text" href="#">About Me</a>
           </li>
           <li>
-            <a href="#">Learning</a>
+            <a href="#" className="nav-text">Learning</a>
           </li>
           <li>
-            <a href="#">Projects</a>
+            <a href="#" className="nav-text">Projects</a>
           </li>
           <li>
-            <a href="#">Socials</a>
+            <a href="#" className="nav-text">Socials</a>
           </li>
       </nav>
     </div>
